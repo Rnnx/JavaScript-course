09f97dc56ba59b0be33889cd5426c37d939ed51b
Karol Martyniuk <rnnxnew@gmail.com>
2022-12-08 20:44:12 +0100

    All practice from section 8.

--- Fundamentals pt.4/js-practice/script.js
+++ Fundamentals pt.4/js-practice/script.js
@@ -82,3 +82,68 @@ var numProducts = 10
 function deleteShoppingCart() {
     console.log("All products deleted!")
 }
+
+// --------------------
+// THIS
+// --------------------
+console.log(this)
+
+const calcDeath = function(birthYear) {
+    console.log(birthYear + 100)
+    console.log(this) // using 'strict mode' makes this keyword undefined in this scope
+}
+calcDeath(1994)
+
+const calcDeathArrow = (birthYear) => {
+    console.log(birthYear + 100)
+    return console.log(this) // this in arrow functions refers to surrounding function. It's called lexical this, and in this example it refers to window
+}
+calcDeathArrow(1991)
+
+const jonas = {
+    year: 1980,
+    calcAge: function() {
+        console.log(this) // this of the method points to the object that called it. In this example the jonas object will be displayed
+    }
+}
+jonas.calcAge()
+
+const matilda = {
+    year: 2017,
+}
+matilda.calcAge = jonas.calcAge // method borrowing
+matilda.calcAge() // it doesn't matter where the function is declared. In this example calcAge() declaration resides inside the jonas object but the object calling it is matilda, therefore this keyword points to matilda object
+
+const f = jonas.calcAge
+// f() - because here we deal with so called simple function call, this keyword is undefined. Trying to console.log(this) will resolve in getting an error
+
+// --------------------
+// THIS IN REGULAR VS ARROW FUNCTIONS
+// --------------------
+jonas.firstName = 'Jonas'
+jonas.greet = () => console.log(`Hey ${this.firstName}`)
+jonas.greet() // once again, because we used this keyword inside an arrow function It is pointing to the surrounding function which in this case refers to window
+
+const addTwo = function(a, b) {
+    console.log(arguments)
+}
+addTwo(3, 5) // regular functions have access to arguments
+
+// --------------------
+// PRIMITIVE VS. REFERENCE TYPES
+// --------------------
+let age = 30
+let oldAge = age
+age = 31
+console.log(age)
+console.log(oldAge)
+
+// because of reference they values change for both objects
+const mySelf = {
+    name: 'Karol',
+    age: 28
+}
+const friend = mySelf
+friend.age = 19
+console.log(`Friend: ${friend.age}`)
+console.log(`Me: ${mySelf.age}`)
