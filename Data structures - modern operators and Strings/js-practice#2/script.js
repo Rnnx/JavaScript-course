bb294c1674bd67ab6d542a14cad15bb8bcf6ec15
Rnnx <rnnxnew@gmail.com>
2022-12-19 20:13:45 +0100

    Update + coding challenge 12.

--- Data structures - modern operators and Strings/js-practice#2/script.js
+++ Data structures - modern operators and Strings/js-practice#2/script.js
@@ -1,5 +1,54 @@
 'use strict';
 
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 13,
+    },
+    sat: {
+      open: 0,
+      close: 24,
+    },
+  },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+  // Real life application of destructuring. When we don't know the order of parameters inside given object! **NOTE! parameters = xx are default values!
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '22:30',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is Your delicious pasta with ${ing1}, ${ing2}, ${ing3}`);
+  },
+  orderPizza: function (mainIngredient, ...otherIngredients) {
+    console.log(mainIngredient);
+    console.log(otherIngredients);
+  },
+};
+
+const { name, openingHours, categories } = restaurant;
+console.log(`name: ${name}
+openingHours: ${openingHours}
+categories: ${categories}`);
+
 // --------------------
 // SETS
 // --------------------
@@ -60,7 +109,7 @@ console.log(rest.size);
 
 // array as key - BELOW EXAMPLE WILL PRODUCE ERROR BECAUSE TWO ARRAYS ARE DIFFERENT OBJECTS
 //                STORED IN HEAP, THEREFORE THE ARRAY WE TRY TO PASS INSIDE GET METHOD IS NOT
-//                THE SAME SAME ARRAY THAT WE DECLARED USING SET METHOD.
+//                THE SAME SAME ARRAY THAT WE DECLARED USING SET METHOD. YES IT DOES.
 // rest.set([1, 2], 'Test')
 // console.log(rest)
 // console.log(rest.get([1, 2]))
@@ -69,3 +118,34 @@ console.log(rest.size);
 const arr = [1, 2];
 rest.set(arr, 'Test');
 console.log(rest.get(arr));
+
+// populating the map
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JS'],
+  ['correct', 3],
+  [true, 'Correct 🏆'],
+  [false, 'Try again'],
+]);
+console.log('karol');
+console.log(question);
+
+// converting objects to map
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// quizz app
+console.log(question.get('question'));
+// looping through map
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt('Your answer'));
+const answer = 3;
+console.log(question.get(answer === question.get('correct')));
+
+// converting map to array
+const mapToArray = [...question];
+console.log(mapToArray);
