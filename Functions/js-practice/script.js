c57d68bbcede71fb4c74d32cc0f879784cdc1dc7
Rnnx <rnnxnew@gmail.com>
2023-01-13 20:03:46 +0100

    Practice update.

--- Functions/js-practice/script.js
+++ Functions/js-practice/script.js
@@ -29,27 +29,27 @@ transformer('Andrzej Sapkowski napisał wiedźmina!', oneWord);
 // Functions returning functions
 // --------------------
 
-const greet = function (greeting) {
-  return function (name) {
-    console.log(`${greeting} ${name}`);
-  };
-};
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
 
-// the value below is essentialy a function
-const greeterHey = greet('Hey');
-// this means that we can call it like a regular function now, eg.
-greeterHey('Jonas');
-greeterHey('Andrzej');
+// // the value below is essentialy a function
+// const greeterHey = greet('Hey');
+// // this means that we can call it like a regular function now, eg.
+// greeterHey('Jonas');
+// greeterHey('Andrzej');
 
-// we can also call it in one go
-greet('Hello')('Andrzej');
+// // we can also call it in one go
+// greet('Hello')('Andrzej');
 
-// making the same function as arrow
-const greetArrow = greeting => {
-  return name => console.log(`${greeting} ${name}`);
-};
+// // making the same function as arrow
+// const greetArrow = greeting => {
+//   return name => console.log(`${greeting} ${name}`);
+// };
 
-greetArrow('Fuck You!')('Arnold');
+// greetArrow('Fuck You!')('Arnold');
 
 // --------------------
 // The call and apply methods
@@ -75,9 +75,9 @@ const eurowings = {
   bookings: [],
 };
 const book = lufthansa.book;
-// Does NOT work
+// does NOT work
 // book(23, 'Sarah Williams');
-// Call method
+// call method
 book.call(eurowings, 23, 'Sarah Williams');
 console.log(eurowings);
 book.call(lufthansa, 239, 'Mary Cooper');
@@ -93,3 +93,71 @@ const flightData = [583, 'George Cooper'];
 book.apply(swiss, flightData);
 console.log(swiss);
 book.call(swiss, ...flightData);
+
+// --------------------
+// The bind method
+// --------------------
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+bookEW(23, 'Steven Williams');
+
+const bookEW23 = book.bind(eurowings, 23);
+bookEW23('Andrzej Sapkowski');
+bookEW23('J.R.R. Tolkien');
+
+// with event listeners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// partial application
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(100));
+
+const greet = function (greeting) {
+  return function (name) {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+// the value below is essentialy a function
+const greeterHey = greet('Hey');
+// this means that we can call it like a regular function now, eg.
+greeterHey('Jonas');
+greeterHey('Andrzej');
+
+// we can also call it in one go
+greet('Hello')('Andrzej');
+
+// making the same function as arrow
+const greetArrow = greeting => {
+  return name => console.log(`${greeting} ${name}`);
+};
+
+greetArrow('Fuck You!')('Arnold');
+
+// const fcfAddTax = rate => {
+//   return {
+//     fcfAddVAT: value => value + value fcfAddTax
+//   }
+// };
+
+const fcfAddTax = rate => {
+  return value => value + value * rate;
+};
+const fcfAddVAT = fcfAddTax(0.23);
+
+console.log(fcfAddVAT(1000));
+
+console.log(fcfAddTax(0.23)(10));
