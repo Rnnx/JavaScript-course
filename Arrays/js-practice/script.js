2a1cfe92a5b6a0b92a8ee012f3fd459fc16a162d
Rnnx <55241375+Rnnx@users.noreply.github.com>
2023-02-08 16:21:54 +0100

    Update script.js

--- Arrays/js-practice/script.js
+++ Arrays/js-practice/script.js
@@ -251,3 +251,32 @@ const overalBalance2 = accounts
   .flatMap(acc => acc.movements)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(overalBalance2);
+
+// --------------------
+// Sorting Arrays
+// --------------------
+
+// strings
+const owners = ['Jonas', 'Zach', 'Adam', 'Martha'];
+console.log(owners.sort());
+console.log(owners);
+// numbers
+console.log(movements);
+// return < 0, A, B (keep order)
+// return > 0, B, A (switch order)
+
+// ascending
+// movements.sort((a, b) => {
+//   if (a > b) return 1;
+//   if (a < b) return -1;
+// });
+movements.sort((a, b) => a - b);
+console.log(movements);
+
+// descending
+// movements.sort((a, b) => {
+//   if (a > b) return -1;
+//   if (a < b) return 1;
+// });
+movements.sort((a, b) => b - a);
+console.log(movements);
