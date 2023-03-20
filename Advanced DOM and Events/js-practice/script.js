f27835618c9e51f2d139c385951bf3f018a98e29
Rnnx <55241375+Rnnx@users.noreply.github.com>
2023-03-20 19:22:46 +0100

    Update script.js

--- Advanced DOM and Events/js-practice/script.js
+++ Advanced DOM and Events/js-practice/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // ---------------------
-// Modal window (like at the beginning of the course)
+// Modal window
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
@@ -122,3 +122,28 @@ const handleHover = function (e) {
     logo.style.opacity = this;
   }
 };
+
+// Passing "argument" into handler
+nav.addEventListener('mouseover', handleHover.bind(0.5));
+nav.addEventListener('mouseout', handleHover.bind(1));
+
+// --------------------
+// Sticky navigation: Intersection Observer API
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  // console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add('sticky');
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
+});
+
+headerObserver.observe(header);
